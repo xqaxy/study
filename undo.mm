3cd203a8d2402dd5b341ae89143dd616ebcfa982
fugq <fugq@95169com>
2012-08-14 13:53:29 +0800

    xx

---
+++ undo.mm
@@ -0,0 +1,39 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1340679029362" ID="ID_1333873617" MODIFIED="1344911389179" TEXT="&#x672a;&#x5b8c;&#x6210;">
+<node CREATED="1343630787751" ID="ID_141399686" MODIFIED="1343698539587" POSITION="right" TEXT="&#x4e0a;&#x6d77;cvs&#x4ee3;&#x7801;&#x8fc1;&#x79fb;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1343630805685" ID="ID_183673564" MODIFIED="1343698534859" POSITION="right" TEXT="&#x81ea;&#x52a8;&#x6258;&#x7ba1;&#x4ee3;&#x7801;&#x6574;&#x7406;"/>
+<node CREATED="1343698518886" ID="ID_1947098024" MODIFIED="1343698526144" POSITION="right" TEXT="cxf &#x5b66;&#x4e60;"/>
+<node CREATED="1343699273703" ID="ID_1170079057" MODIFIED="1343699288843" POSITION="right" TEXT="svn data &#x6539;&#x4e3a;dw"/>
+<node CREATED="1343894318662" ID="ID_1331776171" MODIFIED="1343897048948" POSITION="right" TEXT="&#x5206;&#x9875;&#x903b;&#x8f91;&#x5904;&#x7406;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1344241830349" ID="ID_1419823696" MODIFIED="1344241839065" POSITION="right" TEXT="&#x6d4b;&#x8bd5;&#x5b8c;&#x6210;&#x7684;&#x63a5;&#x53e3;"/>
+<node CREATED="1343897050681" ID="ID_758192260" MODIFIED="1343897067631" POSITION="right" TEXT="&#x8ba2;&#x5355;&#x53d6;&#x6d88;&#x76f8;&#x5173;&#x65f6;&#x95f4;&#x7684;&#x786e;&#x5b9a;"/>
+<node CREATED="1343894324637" ID="ID_1517566670" MODIFIED="1343894327994" POSITION="right" TEXT="&#x9752;&#x5c9b;"/>
+<node CREATED="1343957702247" ID="ID_1401584562" MODIFIED="1343957715736" POSITION="right" TEXT="&#x4e13;&#x5bb6;&#x505c;&#x8bca;&#x63a5;&#x53e3;"/>
+<node CREATED="1343957703713" ID="ID_455459196" MODIFIED="1343963613202" POSITION="right" TEXT="&#x7535;&#x8bdd;&#x95ee;&#x9898;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1344302381035" ID="ID_257354804" MODIFIED="1344302405554" POSITION="right" TEXT="git&#x4e0e;svn &#x5e76;&#x4e14;&#x6574;&#x7406;&#x5b66;&#x4e60;&#x6587;&#x6863;"/>
+<node CREATED="1344302405963" ID="ID_1583452815" MODIFIED="1344302549184" POSITION="right" TEXT="Geronimo &#x6574;&#x7406;&#x4e0e;was&#x7684;&#x5bf9;&#x6bd4;&#xff0c;&#x63a8;&#x5e7f;&#x539f;&#x56e0;"/>
+<node CREATED="1344306007482" ID="ID_1938956992" MODIFIED="1344308871290" POSITION="right" TEXT="&#x6574;&#x7406;sqlreview">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1344308879292" ID="ID_108022055" MODIFIED="1344827104169" POSITION="right" TEXT="&#x81ea;&#x52a8;&#x5316;&#x63a5;&#x53e3;&#x5355;&#x5143;&#x6d4b;&#x8bd5;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1344326650936" ID="ID_1167719111" MODIFIED="1344327018755" POSITION="right" TEXT="sql &#x4f18;&#x5316;&#xff0c;&#x5355;&#x6b21; &#x591a;&#x6b65;&#x64cd;&#x4f5c;"/>
+<node CREATED="1344480856748" ID="ID_359373312" MODIFIED="1344827101391" POSITION="right" TEXT="&#x63a5;&#x53e3;&#x4f7f;&#x7528;&#x5e38;&#x91cf;&#x5b9a;&#x4e49;&#x5728;service&#x4e2d;&#xff0c;dao&#x9700;&#x8981;&#x7684;&#x5e38;&#x91cf;&#x653e;&#x5728;&#x9879;&#x76ee;&#x4e2d;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1344827071465" ID="ID_324774777" MODIFIED="1344827083608" POSITION="right" TEXT="mvn &#x6df1;&#x5165;&#x5b66;&#x4e60;"/>
+<node CREATED="1344827084065" ID="ID_1373541739" MODIFIED="1344827095538" POSITION="right" TEXT="github clone&#x7684;&#x4ee3;&#x7801;&#x6574;&#x7406;"/>
+<node CREATED="1344836453935" ID="ID_531500982" MODIFIED="1344836474965" POSITION="right" TEXT="spring &#x6e90;&#x7801;&#x6df1;&#x5165;&#xff08;DI&#x90e8;&#x5206;&#xff09;"/>
+<node CREATED="1343698609692" ID="ID_745072757" MODIFIED="1343698626450" POSITION="left" TEXT="ora&#x9519;&#x8bef;&#xff0c;&#x6570;&#x636e;&#x5e93;&#x8fde;&#x63a5;&#x6709;&#x95ee;&#x9898;"/>
+<node CREATED="1344235950142" ID="ID_1792098203" MODIFIED="1344235953875" POSITION="left" TEXT="jdk&#x6e90;&#x7801;&#x5b66;&#x4e60;(collection&#x6846;&#x67b6;&#x3001;&#x5e76;&#x53d1;&#x5305;&#x3001;io&#x7b49;)&#xff0c;&#x8981;&#x517b;&#x6210;&#x5e38;&#x770b;jdk&#x6e90;&#x7801;&#x7684;&#x4e60;&#x60ef;&#x3002;  &#x5982;&#x679c;&#x4f60;&#x5bf9;j2ee&#x611f;&#x5174;&#x8da3;&#xff0c;&#x53ef;&#x4ee5;&#x6309;&#x4ee5;&#x4e0b;&#x987a;&#x5e8f;:  tomcat(jetty) -&gt; struts2(spring mvc) -&gt; spring -&gt; ibatis(hibernate)&#x3002;  &#x5982;&#x679c;&#x80fd;&#x575a;&#x6301;&#x8bfb;&#x5b8c;&#x8fd9;&#x4e9b;&#x6838;&#x5fc3;&#x4ee3;&#x7801;&#xff0c;&#x76f8;&#x4fe1;&#x4f60;&#x7684;&#x5883;&#x754c;&#x4f1a;&#x518d;&#x4e0a;&#x4e00;&#x5c42;&#x3002;"/>
+<node CREATED="1344911390036" ID="ID_1706228491" MODIFIED="1344911408627" POSITION="left" TEXT="&#x81ea;&#x52a8;&#x5316;&#x63a5;&#x53e3;&#x5c31;&#x662f;&#x5f52;&#x6211;&#xff0c;&#x5fc5;&#x987b;&#x4ee3;&#x7801;&#x5403;&#x900f;"/>
+</node>
+</map>
